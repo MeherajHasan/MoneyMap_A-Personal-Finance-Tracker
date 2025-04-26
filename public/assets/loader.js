dc390a67c32783f0ea9e81d388f62b6c6e3d26fd
MeherajHasan <meherajhasan2002@gmail.com>
2025-04-26 16:44:46 +0600

    index

---
+++ public/assets/loader.js
@@ -0,0 +1,11 @@
+// loader.js
+
+// Wait until the page is fully loaded
+window.addEventListener('load', () => {
+    // Hide the loader
+    document.getElementById('loader').style.opacity = '0';
+    document.getElementById('loader').style.visibility = 'hidden';
+    
+    // Add the 'loaded' class to body
+    document.body.classList.add('loaded');
+});
